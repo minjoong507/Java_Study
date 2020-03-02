295bc79aa978d985f32a793a65fe162ade111dca
minjoong <albert507@naver.com>
2020-03-02 20:04:43 +0900

    미로 탐색

--- src/Q_2178.java
+++ src/Q_2178.java
@@ -1,6 +1,8 @@
 import java.io.*;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Q_2178 {
@@ -25,11 +27,52 @@ public class Q_2178 {
             String line = st.nextToken();
             for (int j = 0; j < M; j++) {
                 map[i][j] = line.charAt(j) - '0';
+                visited[i][j] = false;
             }
         }
-    	
+    	bfs(0,0);
     }
 
+	public static void bfs(int x_start, int y_start) throws Exception{
+		Queue<int[]> deque = new LinkedList<int[]>();
+		Queue<Integer> distance = new LinkedList<Integer>();
+
+		int[] start = {x_start, y_start};
+		visited[x_start][y_start] = true;
+		deque.add(start);
+		distance.add(1);
+		
+		
+		while (!deque.isEmpty()) {
+			int[] node = deque.poll();
+			int now_distance = distance.poll();
+			int x = node[0];
+			int y = node[1];
+			
+			if (x == N-1 && y == M-1 && visited[x][y]) {
+				System.out.println(now_distance);
+				break;
+			}
+			
+			
+			for (int i =0;i<4;i++) {
+				int next_x = x + dr[i];
+				int next_y = y + dc[i];
+				
+				 if (next_x < 0 || next_y < 0 || next_x >= N || next_y >= M) {
+	                    continue;
+	                }
+				 
+				if (map[next_x][next_y] == 1 && visited[next_x][next_y] == false) {
+					int[] next = {next_x, next_y};
+					deque.add(next);
+					distance.add(now_distance + 1);
+					visited[next_x][next_y] = true;
+				}
+			}
+		
+		}
+	}
     
         
     
