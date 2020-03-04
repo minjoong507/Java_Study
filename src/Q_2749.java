9ddf6c81da581301f6a55aecf7e26e27235ee9a3
minjoong <albert507@naver.com>
2020-03-04 15:12:09 +0900

    피보나치수 3

---
+++ src/Q_2749.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q_2749 {
+	static int i;
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
+
+		long r = sc.nextLong();
+		sc.close();
+		
+		int period = 1500000;
+		long[] fibo = new long[period];
+		fibo[0] = 0;
+		fibo[1] = 1;
+		long ans = r % period;
+		
+		for (i=2;i<fibo.length;i++) {
+			fibo[i] = (fibo[i-1] + fibo[i-2]) % 1000000;
+			if (i==ans)
+				break;
+		}
+		System.out.println(fibo[i]);
+		
+		
+		
+	}
+}
