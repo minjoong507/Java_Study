0e1ee6d70cc3683d5c263cb5419418703a2a51cd
minjoong <albert507@naver.com>
2020-03-04 15:54:21 +0900

    런타임 수정

--- src/Q_10870.java
+++ src/Q_10870.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
 public class Q_10870 {
-	
-	public static void main(String args[]) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int[] arr = new int[A+1];
-		arr[0] =0;
-		arr[1] =1;
-		
-		if (A ==0)
-			System.out.println(arr[0]);
-		else if (A==1)
-			System.out.println(arr[1]);
-		else {
-			for(int i=2;i<A+1;i++) {
-				arr[i] = arr[i-1] + arr[i-2];
-			}
-			
-			System.out.println(arr[A]);
+	public static int fibo(int x) {
+		if (x == 0) {
+			return 0;
+		} else if (x == 1) {
+			return 1;
+		} else {
+			return fibo(x - 1) + fibo(x - 2);
 		}
 
-		
-		
+	}
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		sc.close();
+		System.out.println(fibo(A));
+
 	}
 }
