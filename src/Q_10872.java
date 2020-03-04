6a7d117dcf45820e174cfe193c3a745fc52f0448
minjoong <albert507@naver.com>
2020-03-04 15:40:54 +0900

    팩토리얼

---
+++ src/Q_10872.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Q_10872 {
+	public static int fact(int x) {
+		if (x == 1) {
+			return 1;
+		} else {
+			return (int)(fact(x - 1) * x);
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		sc.close();
+		if (A == 0) {
+			System.out.println(1);
+		}
+		else {
+			int ans = fact(A);
+			System.out.println(ans);
+		}
+
+	}
+}
