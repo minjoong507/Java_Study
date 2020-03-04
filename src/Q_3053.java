e10e114f83231e67bb0d1a4a768dc03213fc6732
minjoong <albert507@naver.com>
2020-03-04 14:43:21 +0900

    택시 기하학

---
+++ src/Q_3053.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q_3053 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		double r = sc.nextDouble();
+
+		System.out.println(Math.pow(r, 2) * Math.PI);
+
+		System.out.println(Math.pow(r, 2) * 2);
+
+	}
+}
