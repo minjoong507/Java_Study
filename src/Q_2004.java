5fa6b925fd58cd73285128530ed65c0fb152711e
minjoong <albert507@naver.com>
2020-03-05 16:08:38 +0900

    조합 0의 개수

---
+++ src/Q_2004.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Q_2004 {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		long B = sc.nextLong();
+		long A = sc.nextLong();
+		long C = B-A;
+		
+		long five = fivesearch(B);
+		long two = twosearch(B);
+		System.out.println(five);
+		long five_2 = fivesearch(A);
+		long two_2 = twosearch(A);
+		
+		
+		long five_3 = fivesearch(C);
+		long two_3 = twosearch(C);
+		
+		
+		System.out.println(Math.min(five - (five_2 + five_3),
+				two - (two_2 + two_3)));	
+	}
+	
+	public static long fivesearch(long x) {
+		long count= 0;
+		
+		for(long i = 5; i<=x; i= i*5)
+			count += x/i;
+		
+		return count;
+	}
+	
+	public static long twosearch(long x) {
+		long count= 0;
+		
+		for(long i = 2; i<=x; i= i*2)
+			count += x/i;
+		
+		return count;
+	}
+	
+}
