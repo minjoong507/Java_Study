04b5c432a8cfa2728ea3341295f523c6f226897e
minjoong <albert507@naver.com>
2020-03-12 17:15:19 +0900

    수정사항 반영

--- src/Q_7568.java
+++ src/Q_7568.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 public class Q_7568 {
-	static int now_tall = 0;
-	static int now_weight = 0;
-
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
