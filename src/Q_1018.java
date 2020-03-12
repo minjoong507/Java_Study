7e441ff4bd0fe2364bffc761c2d956d6003194b1
minjoong <albert507@naver.com>
2020-03-12 17:14:45 +0900

    체스판 다시 칠하기

--- src/Q_1018.java
+++ src/Q_1018.java
@@ -1,51 +1,59 @@
-import java.io.*;
-
+import java.util.Scanner;
+ 
 public class Q_1018 {
-	public static void main (String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] NM = br.readLine().split(" ");
-		int N = Integer.parseInt(NM[0]);
-		int M = Integer.parseInt(NM[1]);
-		int result = 0;
-		int result1 = 0;
+    static int n,m;
+    static int[][] arr;
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt(); 
+        m = sc.nextInt(); 
+        arr = new int[n][m];
+        for(int i=0;i<n;i++) {
+            String t = sc.next();
+            for(int j=0;j<m;j++)
+                arr[i][j] = t.charAt(j);
+        }
+        // 8 * 8 을 살펴보자
+        int result = Integer.MAX_VALUE;
+        for(int i=0;i<=n-8;i++) {
+            for(int j=0;j<=m-8;j++) {
+                result = Math.min(result, solve(i,j));
+            }
+        }
+        System.out.println(result);
+    }
+    
+    private static int solve(int x,int y) {
 
-		char[] mark = {'B', 'W'};
-		char[] mark2 = {'W', 'B'};
-		
-		
-		for(int i=0; i<N; i++) {
-			char[] line = br.readLine().toCharArray();
-			char startmark1 = mark[i%2]; // 0,0 이 B 으로 시작
-			char startmark2 = mark2[i%2]; // 0,0 이 W 으로 시작
-			
-			for(int j=0; j<M; j++) {
-				if (startmark1 == line[j] && j % 2 == 0)
-					continue;
-				else if (startmark1 != line[j] && j % 2 == 1)
-					continue;
-				else
-					result ++;
-			}
-			for(int j=0; j<M; j++) {
-				if (startmark2 == line[j] && j % 2 == 0)
-					continue;
-				else if (startmark2 != line[j] && j % 2 == 1)
-					continue;
-				else
-					result1 ++;
-			}
-		}
-		System.out.println(result);
+        // 1. 시작이 검은색
+        int result = 0;
+        int temp = 'B';
+        for(int i=x;i<x+8;i++) {
+            // 줄의 시작은 이전과 같아야 한다.
+            if(arr[i][y]!=temp) result++;
+            for(int j=y+1;j<y+8;j++) {
+                if(arr[i][j]==temp) {
+                    result++;
 
-		System.out.println(result1);
-		System.out.println(result < result1 ? result : result1);
-		
-		
-		
-		
-		
-		
-		
-	}
+                    if(temp=='B') temp = 'W';
+                    else temp = 'B';
+                }else temp = arr[i][j];
+            }
+        }        
+        // 2. 시작이 흰색
+        int result2 = 0;
+        temp = 'W';
+        for(int i=x;i<x+8;i++) {
+            if(arr[i][y]!=temp) result2++;
+            for(int j=y+1;j<y+8;j++) {
+                if(arr[i][j]==temp) {
+                    result2++;
+                    if(temp=='B') temp = 'W';
+                    else temp = 'B';
+                }else temp = arr[i][j];
+            }
+        }
+        
+        return result>result2?result2:result;
+    }
 }
