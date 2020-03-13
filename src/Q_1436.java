4e9fe01dee0639890dd7a52095e3b21240215c38
minjoong <albert507@naver.com>
2020-03-13 15:30:27 +0900

    영화감독 숌

---
+++ src/Q_1436.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Q_1436 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int temp = 0;
+		String start = "665";
+		
+		while (temp < N) {
+			start = add(start);
+			
+			if (checknum(start)) 
+				temp++;
+
+		};
+		System.out.println(start);
+		
+		
+		
+		
+		
+
+	}
+	
+	
+	public static String add(String x) {
+		return String.valueOf(Integer.parseInt(x)+1);
+	}
+	public static boolean checknum(String x) {
+		int count = 0;
+		for(int i = 0; i<x.length(); i++) {
+			if (x.charAt(i) == '6') 
+				count++;
+			else
+				count = 0;
+			
+			if (count == 3)
+				return true;
+		}
+		return false;
+	}
+	
+}
