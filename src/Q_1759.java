26a3f55952ab7b8bed723e68f070d2a2bf8f4a80
albert507 <albert507@naver.com>
2020-03-26 21:20:13 +0900

    런타임 해결

--- src/Q_1759.java
+++ src/Q_1759.java
@@ -1,49 +1,55 @@
 import java.util.Arrays;
-import java.util.Scanner;
 import java.io.*;
 
 public class Q_1759 {
-	static int L, C;
+	static int L;
+	static int C;
 	static String[] code;
 
 	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		L = sc.nextInt();
-		C = sc.nextInt();
+		
+		String[] input = br.readLine().split(" ");
+		
+		L = Integer.parseInt(input[0]);
+		C = Integer.parseInt(input[1]);
 		
 		code = br.readLine().split(" ");
 		Arrays.sort(code);
-		
+
 		solve(0, "");
 
 	}
 
-    private static void solve(int index, String ret) {
-        if (ret.length() == L) {
-            if (isPossible(ret)) {
-                System.out.println(ret);
-            }
-            return;
-        }
-        if (index >= C) return;
+	public static void solve(int step, String ans) {
 
-        solve(index + 1, ret + code[index]);
-        solve(index + 1, ret);
-    }
-	
+		if (ans.length() == L) {
+			if (isPossible(ans)) {
+				System.out.println(ans);
+			}
+			return;
+		}
+
+		if (step >= C)
+			return;
+
+		solve(step + 1, ans + code[step]);
+		solve(step + 1, ans);
+
+	}
+
 	public static boolean isPossible(String tmp) {
-        int vowel = 0, consonant = 0;
-        for (int i = 0; i < tmp.length(); i++) {
-            if (isVowel(tmp.charAt(i))) vowel++;
-            else consonant++;
-        }
+		int vowel = 0, consonant = 0;
+		for (int i = 0; i < tmp.length(); i++) {
+			if (isVowel(tmp.charAt(i)))
+				vowel++;
+			else
+				consonant++;
+		}
 
-        return vowel >= 1 && consonant >= 2;
-    }
-	
-	
-	
+		return vowel >= 1 && consonant >= 2;
+	}
+
 	public static boolean isVowel(char ch) {
 		return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
 	}
